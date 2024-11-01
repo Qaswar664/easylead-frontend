f187e8e3521d75c10a5461454a560728e48cb206
QaswarAhmad <qaswarwebtechahmad@gmail.com>
2024-11-01 12:09:12 +0500

    change-password

--- src/components/UserAvatar.jsx
+++ src/components/UserAvatar.jsx
@@ -8,6 +8,8 @@ import { getInitials } from "../utils";
 import { toast } from "sonner";
 import { useLogoutMutation } from "../redux/slices/api/authApiSlice";
 import { logout } from "../redux/slices/authSlice";
+import ChangePassword from "./ChangePassword";
+import AddUser from "./AddUser";
 
 const UserAvatar = () => {
   const [open, setOpen] = useState(false);
@@ -90,6 +92,8 @@ const UserAvatar = () => {
           </Transition>
         </Menu>
       </div>
+      <AddUser open={open} setOpen={setOpen} userData={user} />
+      <ChangePassword open={openPassword} setOpen={setOpenPassword} />
     </>
   );
 };
--- src/redux/slices/api/userApiSlice.js
+++ src/redux/slices/api/userApiSlice.js
@@ -34,6 +34,29 @@ export const userApiSlice = apiSlice.injectEndpoints({
         credentials: "include",
       }),
     }),
+    getNotifications: builder.query({
+      query: (data) => ({
+        url: `${USER_URL}/notifications`,
+        method: "GET",
+        credentials: "include",
+      }),
+    }),
+    markNotiAsRead: builder.mutation({
+      query: (data) => ({
+        url: `${USER_URL}/read-noti?isReadType=${data.type}&id=${data?.id}`,
+        method: "PUT",
+        body: data,
+        credentials: "include",
+      }),
+    }),
+    changePassword: builder.mutation({
+      query: (data) => ({
+        url: `${USER_URL}/change-password`,
+        method: "PUT",
+        body: data,
+        credentials: "include",
+      }),
+    }),
   }),
 });
 export const {
@@ -41,4 +64,7 @@ export const {
   useGetTeamListQuery,
   useDeleteUserMutation,
   useUserActionMutation,
+  useGetNotificationsQuery,
+  useMarkNotiAsReadMutation,
+  useChangePasswordMutation,
 } = userApiSlice;
